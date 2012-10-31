35ceb3fe58c4231e29793d62a3b09167a87dd83e
Zachary Wasserman <zachwass2000@gmail.com>
2012-10-31 19:10:41 -0400

    Fixed link handling (in a very basic way)
    Also added stuff from old repo that I thought was in here.

--- worker.py
+++ worker.py
@@ -1,6 +1,9 @@
 import sys
 import os
 import time
+import re
+
+from datetime import datetime, timedelta
 
 #this package seems outdated, but it was super simple to set up
 import tweepy
@@ -14,7 +17,14 @@ FOOTERS = ["#PULAKYELLING"]
 
 ACCOUNTS = ["pulakm", "pennapps"]
 
+#how often to check for new tweets (careful of twitter rate limiting)
 DELAY = 12
+
+#how long until we don't consider a tweet new?
+#this is to deal with tweets bizarrely repeating
+EXPIRED_TIME = timedelta(minutes=2)
+
+URL_RE = ".+\..+" #crude, but should handle most of what Pulak tweets
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -27,13 +37,25 @@ def send_tweet(message):
     for footer in FOOTERS:
         if len(final_message + " " + footer) <= 140:
             final_message += " " + footer
-    final_message = final_message.upper()
     try:
         api.update_status(final_message)
     except Exception as e:
         #this will print to heroku logs
         print e
 
+def to_upper(message):
+    upper_message = []
+    for token in message.split(" "):
+        if is_link(token):
+            print "%s is a link" % token
+            upper_message.append(token)
+        else:
+            upper_message.append(token.upper())
+    return " ".join(upper_message)
+        
+def is_link(token):
+    return True if re.match(URL_RE, token) else False
+        
 if __name__ == "__main__":
     last_ids = {} #keep track of id of last tweet from the stream
     for user in ACCOUNTS:
@@ -48,4 +70,5 @@ if __name__ == "__main__":
                 #no tweeets found for this user
                 continue
             last_ids[user] = tweet.id
-            send_tweet(tweet.text) #Tweet! Tweet!
+            if(datetime.now() - tweet.created_at < EXPIRED_TIME):
+                send_tweet(to_upper(tweet.text)) #Tweet! Tweet!
